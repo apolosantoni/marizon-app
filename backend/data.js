05ec3b95f8e2a778922bfff1452b887eeb3eb554
Apolo O. Santoni <apolosantoni@gmail.com>
2020-11-20 13:58:36 -0300

    install server.js express/nodemon

---
+++ backend/data.js
@@ -0,0 +1,78 @@
+// eslint-disable-next-line no-unused-vars
+const data = {
+    products: [
+        {
+            _id: "1",
+            name: "Nike Slim Shirt",
+            category: "Shirt",
+            image: "/image/p1.jpg",
+            price: 120,
+            countInStock: 1,
+            brand: "Nike",
+            rating: 2.5,
+            numReviews: 10,
+            description: "high quality product"
+        },
+        {
+            _id: "2",
+            name: "Adidas Shirt",
+            category: "Shirt",
+            image: "/image/p2.jpg",
+            price: 100,
+            countInStock: 0,
+            brand: "Adidas",
+            rating: 3.0,
+            numReviews: 10,
+            description: "high quality product"
+        },
+        {
+            _id: "3",
+            name: "Laciste Free Shirt",
+            category: "Shirt",
+            image: "/image/p3.jpg",
+            price: 120,
+            countInStock: 1,
+            brand: "Lacoste",
+            rating: 4.8,
+            numReviews: 17,
+            description: "high quality product"
+        },
+        {
+            _id: "4",
+            name: "Nike Slim Pants",
+            category: "Pants",
+            image: "/image/p4.jpg",
+            price: 78,
+            countInStock: 1,
+            brand: "Nike",
+            rating: 5,
+            numReviews: 14,
+            description: "high quality product"
+        },
+        {
+            _id: "5",
+            name: "Puma Slim Pant",
+            category: "Pants",
+            image: "/image/p5.jpg",
+            price: 65,
+            countInStock: 1,
+            brand: "Puma",
+            rating: 2,
+            numReviews:10,
+            description: "high quality product"
+        },
+        {
+            _id: "6",
+            name: "Adidas Slim Pant",
+            category: "Pants",
+            image: "/image/p6.jpg",
+            price: 139,
+            countInStock: 1,
+            brand: "Adidas",
+            rating: 4.5,
+            numReviews: 15,
+            description: "high quality product"
+        },
+    ],
+};
+export default data;
