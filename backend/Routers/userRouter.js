2cca3d58d8326e3f4c1c93ed7e3c216da9213c06
Apolo O. Santoni <apolosantoni@gmail.com>
2020-11-21 20:12:14 -0300

    connect mongodb configure e set user

--- backend/Routers/userRouter.js
+++ backend/Routers/userRouter.js
@@ -1,12 +1,15 @@
 import express from 'express';
+import expressAsyncHandler from 'express-async-handler';
 import data from '../data.js';
 import User from '../models/userModel.js';
 
 const userRouter = express.Router();
 
-userRouter.get('/seed',async (req, res) => {
+userRouter.get('/seed',
+ expressAsyncHandler( async (req, res) => {
+     await User.remove({});
     const createUsers = await User.insertMany(data.users);
     res.send({createUsers});
-});
+}));
 
 export default userRouter;
