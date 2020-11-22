bd1eb55d654a1f2c32a57a16c095da39a3e9fba8
Apolo O. Santoni <apolosantoni@gmail.com>
2020-11-22 08:09:58 -0300

    create sign in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,42 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { BrowserRouter, Link, Route} from 'react-router-dom';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import SigninScreen from './screens/SigninScreen';
 
 
 function App() {
-  const cart = useSelector((state)=> state.cart);
-  const {cartItems} = cart;
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
   return (
     <BrowserRouter>
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <Link className="brand" to="/"> marizon </Link>
-        </div>
-        <div>
-          <Link to="/cart">Cart{
-            cartItems.length > 0 &&(
-            <span className="badge">{cartItems.length}</span>
-            )
-          }
-          </Link>
-          <Link to="/singin">Sing In</Link>
-        </div>
-      </header>
-      <main>
-        <Route path='/cart/:id?' component={CartScreen}></Route>
-        <Route path='/product/:id' component={ProductScreen}></Route>
-        <Route path='/' component={HomeScreen} exact></Route>
-      </main>
-      <footer className="row center">
-        All right reserved
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <Link className="brand" to="/"> marizon </Link>
+          </div>
+          <div>
+            <Link to="/cart">Cart{
+              cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )
+            }
+            </Link>
+            <Link to="/signin">Sing In</Link>
+          </div>
+        </header>
+        <main>
+          <Route path='/cart/:id?' component={CartScreen}></Route>
+          <Route path='/product/:id' component={ProductScreen}></Route>
+          <Route path='/signin' component={SigninScreen}></Route>
+          <Route path='/' component={HomeScreen} exact></Route>
+        </main>
+        <footer className="row center">
+          All right reserved
       </footer>
-    </div>
+      </div>
     </BrowserRouter>
   );
 }
