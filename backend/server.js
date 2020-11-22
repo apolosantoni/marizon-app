110079274faf5a6cfff740ff9d82f42da98e13dc
Apolo O. Santoni <apolosantoni@gmail.com>
2020-11-22 03:56:18 -0300

    user signin , jwt, postman config

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,20 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import userRouter from './Routers/userRouter.js';
 import productRouter from './Routers/productRouter.js';
 
+dotenv.config();
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/marizon-app', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
 })
-
-
 
 /*
 // use to local file data.js
